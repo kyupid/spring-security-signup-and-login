2c07433ff384eb3a6b371c7110fc3a05e2b2ceb9
kyupid <iamscvv@gmail.com>
2021-12-29 10:58:52 +0900

    로그아웃을 할 수 있다

--- src/main/java/com/example/demo/security/UserController.java
+++ src/main/java/com/example/demo/security/UserController.java
@@ -1,8 +1,14 @@
 package com.example.demo.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @RequiredArgsConstructor
 @Controller
@@ -13,6 +19,12 @@ public class UserController {
     @PostMapping("/user")
     public String signup(UserInfoDto infoDto) { // 회원 추가
         userService.save(infoDto);
+        return "redirect:/login";
+    }
+
+    @GetMapping(value = "/logout")
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, SecurityContextHolder.getContext().getAuthentication());
         return "redirect:/login";
     }
 }
