797e996948d810ba26052321009a1111e639a8d3
kyupid <iamscvv@gmail.com>
2021-12-29 14:58:53 +0900

    DDL 쿼리 초기화

---
+++ src/main/resources/query.sql
@@ -0,0 +1,14 @@
+drop table if exists user_info;
+
+-- auto-generated definition
+create table user_info
+(
+    code     bigint auto_increment
+        primary key,
+    email    varchar(100) null,
+    password varchar(100) null,
+    auth     varchar(255) null,
+    constraint user_info_email_uindex
+        unique (email)
+);
+
