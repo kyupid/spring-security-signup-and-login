22889615f27d0b613a833af9625f6a20df976149
kyupid <iamscvv@gmail.com>
2022-04-07 15:12:03 +0900

    rememer-me Security Config에 설정

--- src/main/java/com/example/demo/WebSecurityConfig.java
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -30,14 +30,23 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter { // 2
                 .antMatchers("/admin").hasRole("ADMIN") // ADMIN만 접근 가능
                 .anyRequest().authenticated() // 나머지 요청들은 권한의 종류에 상관 없이 권한이 있어야 접근 가능
                 .and()
+
+                .rememberMe()
+                .key("uniqueAndSecret")
+                .rememberMeParameter("remember-me")
+                .tokenValiditySeconds(86400 * 14)
+                .and()
+
                 .formLogin() // 7
                 .loginPage("/login") // 로그인 페이지 링크
                 .defaultSuccessUrl("/") // 로그인 성공 후 리다이렉트 주소
                 .and()
+
                 .logout() // 8
                 .logoutSuccessUrl("/login") // 로그아웃 성공시 리다이렉트 주소
                 .invalidateHttpSession(true) // 세션 날리기
         ;
+
     }
 
     @Override
